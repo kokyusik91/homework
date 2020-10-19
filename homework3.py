83605b35108c2a2ef627772f6beab36f64d28dc3
kokyusik91 <63444730+kokyusik91@users.noreply.github.com>
2020-10-19 22:46:33 +0900

    add db homework3.py
    mongo db에 추가하는 코드입니다.

--- homework3.py
+++ homework3.py
@@ -1,5 +1,9 @@
+from pymongo import MongoClient
 import requests
 from bs4 import BeautifulSoup
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
@@ -16,4 +20,9 @@ for tr in trs:
     artist = tr.select_one('td.info > a.artist.ellipsis').text
     print(rank,title,artist)
 
-    # 숙제제출
+    doc = {
+            'rank': rank,
+            'title': title,
+            'artist': artist,
+            }
+    db.trs.insert_one(doc)
